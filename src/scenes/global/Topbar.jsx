a050ec53d1e017490b257ab0e3aff33a00e9269d
TULIMAT Anas OBS/GDO <TZKM2460@orange.com>
2023-10-31 15:47:55 +0100

    feat: add topbar componenet and use the fonctionality of toggle dark or light mode

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Box, IconButton, InputBase, useTheme} from "@mui/material";
+import {ColorModeContext, tokens} from "../../theme";
+import SearchIcon from "@mui/icons-material/Search";
+import {
+    DarkModeOutlined,
+    LightModeOutlined,
+    NotificationsOutlined,
+    PersonOutline,
+    SettingsOutlined
+} from "@mui/icons-material";
 
 const Topbar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
     return (
-        <div>
-            Topbar
-        </div>
+        <Box display="flex" justifyContent="space-between" p={2}>
+            {/* Search Bar */}
+            <Box
+                display="flex"
+                backgroundColor={colors.primary[400]}
+                borderRadiux="3px"
+            >
+                <InputBase sx={{ml: 2, flex: 1}} placeholder="Search..."/>
+                <IconButton type="button" sx={{p: 1}}>
+                    <SearchIcon/>
+                </IconButton>
+            </Box>
+
+            {/* Icons */}
+            <Box display="flex">
+                <IconButton onClick={colorMode.toggleColorMode}>
+                    {theme.palette.mode === 'dark' ? (
+                        <DarkModeOutlined/>
+                    ) : <LightModeOutlined/>}
+
+                </IconButton>
+                <IconButton>
+                    <NotificationsOutlined/>
+                </IconButton>
+
+                <IconButton>
+                    <SettingsOutlined/>
+                </IconButton>
+
+                <IconButton>
+                    <PersonOutline/>
+                </IconButton>
+
+            </Box>
+
+        </Box>
     );
 };
 
